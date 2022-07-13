541ebd649d044ca474ffcc28f544bdfca14a17de
Paulo <paulo.ricardo1137.pr@gmail.com>
2022-07-13 16:54:32 +0100

    Add funciotion to verify 5 min ago

--- core/views.py
+++ core/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from .models import Double
+from datetime import datetime, timedelta
+import datetime
+import django
 
 def name_valid(name):
-    alpha = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    alpha = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ '
     for c in name:
         if c not in alpha:
             return False
@@ -23,8 +26,11 @@ def double(request):
         error = 'erro charactere'
         return render(request, 'double.html', {'doubles': doubles, 'error': error})
 
+    five_minutes_ago = django.utils.timezone.now() + datetime.timedelta(minutes=-5)
+    result_five_min_ago = Double.objects.filter(name=person_name, value=value, date__gte=five_minutes_ago)
+
     already_exist = Double.objects.filter(name=person_name, value=value)
-    if already_exist.count() == 0:
+    if already_exist.count() == 0 or result_five_min_ago.count() == 0:
         Double.objects.create(name=person_name, value=value, double_value=value*2)
 
     return render(request, 'double.html', {'doubles': doubles})
